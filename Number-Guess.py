82258e1949364271fd964927693979f3a9e8f537
Sanjay Kumar <sanjaykumarvtht@gmail.com>
2022-10-22 13:16:32 +0530

    Create Number-Guess.py

--- Number-Guess.py
+++ Number-Guess.py
@@ -4,11 +4,11 @@ num = random.randint(1, 10)
 guess = None
 
 while guess != num:
-    guess = input("guess a number between 1 and 10: ")
+    guess = input("Guess a number between 1 and 10: ")
     guess = int(guess)
 
     if guess == num:
-        print("congratulations! you won!")
+        print("Congratulations! You won!")
         break
     else:
-        print("nope, sorry. try again!")
+        print("Nope, Try Again!")
